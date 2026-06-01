f09bbdc84fc5bb5add1878f340b4db1455e80cf0
maxim.ananyev <maksim.ve@gmail.com>
2026-06-01 18:36:37 +0300

    Подключит моки фильтров

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import FiltersView from './view/filters-view.js';
 import {render} from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
+import {generateFilter} from './mock/filter.js';
 
 const tripMainElement = document.querySelector('.trip-main');
 const filtersContainerElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
+const filters = generateFilter(pointsModel.points);
+
 const boardPresenter = new BoardPresenter({
   boardContainer: tripEventsElement,
   pointsModel,
 });
 
-render(new FiltersView(), filtersContainerElement);
+render(new FiltersView({filters}), filtersContainerElement);
 boardPresenter.init();
