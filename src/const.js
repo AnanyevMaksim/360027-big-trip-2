5c0bcbbea8e0434195a399605ffc58876b90ae7d
maxim.ananyev <maksim.ve@gmail.com>
2026-07-03 09:32:56 +0300

    Подключит API и обучит модель получать данные с сервера

--- src/const.js
+++ src/const.js
@@ -33,6 +33,7 @@ const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR',
+  INIT: 'INIT',
 };
 
 export {POINT_TYPES, FilterType, SortType, UserAction, UpdateType};
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import BoardPresenter from './presenter/board-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
+import PointsApiService from './points-api-service.js';
+
+const AUTHORIZATION = 'Basic gV3hQ5aAo7sL4vK';
+const END_POINT = 'https://22.objects.htmlacademy.pro/big-trip';
 
 const tripMainElement = document.querySelector('.trip-main');
 const filtersContainerElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 const newPointButtonElement = tripMainElement.querySelector('.trip-main__event-add-btn');
 
-const pointsModel = new PointsModel();
+const pointsModel = new PointsModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION),
+});
 const filterModel = new FilterModel();
 
 const boardPresenter = new BoardPresenter({
@@ -37,3 +43,4 @@ newPointButtonElement.addEventListener('click', handleNewPointButtonClick);
 
 filterPresenter.init();
 boardPresenter.init();
+pointsModel.init();
